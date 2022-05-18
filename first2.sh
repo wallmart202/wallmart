88a93ee7e9596c08e37b150ea1c410bf45fd0d54
dharmaraju313 <dharmaraju1603@gamil.com>
2022-05-18 18:27:24 +0530

    my first

---
+++ first2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Hello      World"       # This is a comment, too!
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
